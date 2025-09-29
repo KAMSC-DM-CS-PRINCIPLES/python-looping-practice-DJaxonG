66c960cc304d0beb6eeb65ebe7d1d370a83f5bbb
DJaxonG <djaxong33@gmail.com>
2025-09-29 13:06:16 -0400

    Update multiplication_table.py

--- multiplication_table.py
+++ multiplication_table.py
@@ -7,7 +7,7 @@ def multiplication_table(n):
     for i in range(1, n + 1):
         for j in range(1, n + 1):
             mult = i*j
-            print(i ""
+            print(i + "*" + j + "=" + mult)
 
 if __name__ == "__main__":
     # create multiplication table below this
